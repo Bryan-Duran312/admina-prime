58079ae6f943e3b7d0ae477a6c2aed7cc159b2e3
Bryan Daniel Duran Duran <bryanduran31208@email.com>
2026-08-19 10:09:05 -0500

    Fix de ruta de escritura SQLite a /tmp para Vercel

--- admina_prime_core/settings.py
+++ admina_prime_core/settings.py
@@ -79,7 +79,7 @@ if 'VERCEL' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': '/tmp/db.sqlite3',
         }
     }
 else:
--- admina_prime_core/wsgi.py
+++ admina_prime_core/wsgi.py
@@ -15,3 +15,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admina_prime_core.settings')
 
 application = get_wsgi_application()
 app = application
+
+if 'VERCEL' in os.environ and not os.path.exists('/tmp/db.sqlite3'):
+	from django.core.management import call_command
+
+	call_command('migrate', interactive=False, verbosity=0)
